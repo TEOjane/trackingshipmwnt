dbb855c03f507b0af746c7025e476cee69bcc893
Евгения Тулузакова <zeka.009@mail.ru>
2024-04-05 10:11:57 +0300

    added app service methods

--- Tracking/Application/IApplicationService.cs
+++ Tracking/Application/IApplicationService.cs
@@ -4,4 +4,15 @@ namespace Application;
 
 public interface IApplicationService
 {
+    decimal CalcShippingCost(Cargo cargo);
+
+    void GetLocation(Cargo cargo);
+
+    Cargo GetCargoInfo(long cargoId);
+
+    long AddCargo(Cargo cargo);
+
+    Carrier GetCarrierInfo(long carrierId);
+
+    Truck GetTruckInfo(string truckNumber);
 }
